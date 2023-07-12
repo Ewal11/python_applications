d9c94a3be99d3a2be65c6480502c1401a67aeb47
dhruvcodes123 <dhruvpatel167413@gmail.com>
2023-07-12 19:09:08 +0530

    feat: Add comments and improvements to the code provided. - Change Logger to custom Logger. - Update in minor python conventions. - Other conditional formatting. - Add Exception handling.

--- schemas.py
+++ schemas.py
@@ -5,14 +5,14 @@ from datetime import datetime
 
 
 class HodlHodlOfferBase(BaseModel):
-
     trading_type_name: str
     trading_type_slug: str
     # We will query our database depending on this key
     coin_currency: str
     fiat_currency: str
-    payment_method_slug: str
-    payment_method_name: str
+    # Need to make below type optional as it may return None
+    payment_method_slug: Optional[str]
+    payment_method_name: Optional[str]
 
     country_code: str
     min_trade_size: str
@@ -24,6 +24,7 @@ class HodlHodlOfferBase(BaseModel):
     site_name: str
 
     headline: str
+
 
 class HodlHodlUserBase(BaseModel):
     @classmethod
@@ -50,11 +51,19 @@ class HodlHodlUserBase(BaseModel):
 
 
 class Settings(BaseSettings):
-    
-
     class Config:
         env_file = '.env'
         env_file_encoding = "utf-8"
 
 
 settings = Settings()
+
+
+class LogConfiguration:
+    logger_name: str = "Python Application-main"
+    # The below format represents: timestamp of log, log level (INFO, ERROR, ...), name of logger, path of file where
+    # error occurred, line number, function name, and the custom message we added
+    logger_formatter: str = "%(asctime)s-%(levelname)s-%(name)s-%(process)d-%(pathname)s|%(lineno)s:: %(funcName)s|%(" \
+                            "lineno)s:: %(message)s "
+    # Name of file where logs will be created
+    log_file_base_name: str = "log"
